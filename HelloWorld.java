08347425652cfad217f437a8996c25bc8c67b30a
XuantongLu <luxuantong1994@gmail.com>
2019-04-09 14:09:11 +0200

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package quickstart;
+
+/**
+ * Created by Xuantong on 4/9/2019.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+
+}
